87e55f7f851dc4e693b766bc38e40c8027779cba
Bret Baker <bretbaker808@gmail.com>
2020-04-24 10:32:42 -0700

    string to int

--- get-first-value.java
+++
@@ -1,6 +0,0 @@
-class Challenge {
-  public static void main(String[] args) {
-    int[] arr = new int[] { 1, 2, 3, 4, 5 };
-    System.out.println(arr[0]);
-  }
-}
---
+++ stringtointeger/Challenge.java
@@ -0,0 +1,10 @@
+package stringtointeger;
+
+import java.util.Arrays;
+
+public class Challenge {
+  public static void main(final String[] args) {
+    String[] arr = new String[] { "1", "2", "3" };
+    System.out.println(Arrays.toString(arr));
+  }
+}
