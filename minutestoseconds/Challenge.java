c69de3c29e5532d3bfe67e5de79f517a16d9d3b7
Bret Baker <bretbaker808@gmail.com>
2020-04-25 09:20:09 -0700

    minutestoseconds

---
+++ minutestoseconds/Challenge.java
@@ -0,0 +1,11 @@
+package minutestoseconds;
+
+public class Challenge {
+  public static void main(String[] args) {
+    System.out.println(convert(5));
+  }
+
+  public static int convert(int minutes) {
+    return minutes * 60;
+  }
+}
