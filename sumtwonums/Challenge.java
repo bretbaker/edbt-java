29b526a5cd589203368f5bdc8f9ed2484157d69b
Bret Baker <bretbaker808@gmail.com>
2020-04-28 13:55:06 -0700

    sumtwonums

---
+++ sumtwonums/Challenge.java
@@ -0,0 +1,11 @@
+package sumtwonums;
+
+public class Challenge {
+  public static void main(String[] args) {
+    System.out.println(sumTwoNums(5, 10));
+  }
+
+  public static int sumTwoNums(int a, int b) {
+    return a + b;
+  }
+}
