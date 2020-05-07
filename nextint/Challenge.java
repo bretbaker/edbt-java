546655722ae9d767b6b80c106038aa5c61cf2043
Bret Baker <bretbaker808@gmail.com>
2020-05-06 17:49:18 -0700

    nextint

---
+++ nextint/Challenge.java
@@ -0,0 +1,11 @@
+package nextint;
+
+public class Challenge {
+  public static void main(String[] args) {
+    System.out.println(addition(7));
+  }
+
+  public static int addition(int num) {
+    return ++num;
+  }
+}
