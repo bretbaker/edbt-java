07ccf8e225b96e5a06e9caf9f04419309221395d
Bret Baker <bretbaker808@gmail.com>
2020-05-09 19:17:09 -0700

    remainder

---
+++ remainder/Challenge.java
@@ -0,0 +1,11 @@
+package remainder;
+
+public class Challenge {
+  public static void main(String[] args) {
+    System.out.println(remainder(5, 11));
+  }
+
+  public static int remainder(int a, int b) {
+    return b % a;
+  }
+}
