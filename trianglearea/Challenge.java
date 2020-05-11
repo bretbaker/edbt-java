555d8de8e585cd2aed7c4da8ace4860f089f08f8
Bret Baker <bretbaker808@gmail.com>
2020-05-10 19:48:28 -0700

    trianglearea

---
+++ trianglearea/Challenge.java
@@ -0,0 +1,11 @@
+package trianglearea;
+
+public class Challenge {
+  public static void main(String[] args) {
+    System.out.println(triArea(7, 7));
+  }
+
+  public static double triArea(double base, double height) {
+    return (base * height) / 2;
+  }
+}
