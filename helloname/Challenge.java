2a9bac60d922f4085882bedc1d5c39ac7c899576
Bret Baker <bretbaker808@gmail.com>
2020-05-24 22:04:07 -0700

    helloname

---
+++ helloname/Challenge.java
@@ -0,0 +1,10 @@
+package helloname;
+
+public class Challenge {
+  public static void main(String[] args) {
+    System.out.println(helloName("karen"));
+  }
+  public static String helloName(String name) {
+		return "Hello " + name;
+	}
+}
