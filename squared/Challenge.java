6d20e442970113f235b913dedd9ec15255ea2fea
Bret Baker <bretbaker808@gmail.com>
2020-05-30 10:19:10 -0700

    squared

---
+++ squared/Challenge.java
@@ -0,0 +1,11 @@
+package squared;
+
+public class Challenge {
+  public static void main(String[] args) {
+    System.out.println(squared(8));
+  }
+
+  public static int squared(int b) {
+    return b * b;
+  }
+}
